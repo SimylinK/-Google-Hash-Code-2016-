fb7eeb3e55ec8bb772371d57a81db6e6c44202e5
MARAVAL Nathan <E146215E@polytech-OptiPlex-7010>
2016-11-09 12:51:57 +0100

    création dossier docs et exemplesDonnes

---
+++ exemplesDonnes/polyhash/polyhmodel.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Module de définition des structures de données de Poly#.
+"""
+
+# ajouter dans cette liste tous les symboles 'importables'
+__all__ = ['Useless']
+
+
+class Useless:
+    """
+        Une classe vraiment inutile.
+    """
+    def __str__(self):
+        return "Poly# vide"
+
+
+if __name__ == "__main__":
+    inutile = Useless()
+    print('J\'ai créé un {}.'.format(inutile))
