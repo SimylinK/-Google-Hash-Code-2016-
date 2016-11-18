e01567d559b96d467e935f451f2cbf8ea33a854a
MARAVAL Nathan <E146215E@polytech-OptiPlex-7010>
2016-11-18 15:21:26 +0100

    création classe Satellite

---
+++ Classes/Satellite.py
@@ -0,0 +1,8 @@
+class Satellite:
+    def __init__(self, latitudeDepart, longitudeDepart, vitesse,
+                 vitesseCamera, maxDeplacementCamera):
+        self.latitudeDepart = latitudeDepart
+        self.longitudeDepart = longitudeDepart
+        self.vitesse = vitesse
+        self.vitesseCamera = vitesseCamera
+        self.maxDeplacementCamera = maxDeplacementCamera
