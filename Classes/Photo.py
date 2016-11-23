5083ec6dc25a9477bafe0b16955ca3da3f5b0804
Mathilde Monvoisin <mathilde.monvoisin@etu.univ-nantes.fr>
2016-11-23 16:51:11 +0100

    Ajout docstring sur la classe Photo

--- Classes/Photo.py
+++ Classes/Photo.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 class Photo:
+    """
+        Classe composée de :
+        La latitude et la longitude de chaque photo.
+    """
+    
     def __init__(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
