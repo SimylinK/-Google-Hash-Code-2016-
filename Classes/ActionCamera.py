89f56afa62977d046cae99210ba66e510f237a20
Mathilde Monvoisin <mathilde.monvoisin@etu.univ-nantes.fr>
2016-11-23 18:54:22 +0100

    Ajout de ActionCamera dans diagramme_avancement.dia + création ActionCamera sauf méthode get_photo

---
+++ Classes/ActionCamera.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from Photo import Photo
+
+class ActionCamera:
+    """
+        Classe composée de :
+        Un certain nombre de photos qu'il faudra prendre pour avoir les
+        points qui lui sont associés. 
+    """
+
+    def __init__(self, tour, mouvement_latitude = None,
+                 mouvement_longitude = None, photo):
+        self.tour = tour
+        self.mouvement_latitude = mouvement_latitude
+        self.mouvement_longitude = mouvement_longitude
+        self.photo = self.get_photo()
+
+
+    def get_photo(self):
+        return "blop" #je sais pas encore comment faire
+        
