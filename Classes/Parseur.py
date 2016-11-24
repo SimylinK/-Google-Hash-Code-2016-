98b5f80ce943842a1dca3809ad149a43e31db142
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2016-11-24 19:12:49 +0100

    Optimisation Parseur

--- Classes/Parseur.py
+++ Classes/Parseur.py
@@ -24,72 +24,63 @@ class Parseur:
 
     def __init__(
             self, chemin_input=None, chemin_output=None):
-        """Le fichier d'input original est converti en fichier pickle"""
         self.chemin_input = self.demander_input()
         self.chemin_output = self.demander_output()
 
     def demander_input(self):
         chemin = ' '
-        while chemin[0] != '/' and chemin[0] != 'C':  # Vérification que le chemin est bien absolu.
+        while chemin == ' ':  # Vérification que le chemin n'est pas laissé vide
             chemin = input("Chemin absolu du fichier input : ")
         return chemin
 
     def demander_output(self):
         chemin = ' '
-        while chemin[0] != '/' and chemin[0] != 'C':  # Vérification que le chemin est bien absolu.
+        while chemin == ' ':  # Vérification que le chemin n'est pas laissé vide
             chemin = input("Chemin absolu du fichier output : ")
         return chemin
 
     def recup(self):
-        """Méthode chargée de :
-        Récupérer le nombre de tours et le nombre de satellites
-        Lancer la création des instances de la classe Satellite
+        """Méthode chargée de : Récupérer les informations du fichier d'input.
         """
 
         fichier_input = open(self.chemin_input, 'r')
-        nb_tours = fichier_input.readline().rstrip()  # rstrip est utilisé pour ne pas prendre "\n" en compte.
-        nb_satellites = fichier_input.readline().rstrip()
+        nb_tours = int(fichier_input.readline().rstrip()) # rstrip est utilisé pour ne pas prendre "\n" en compte.
+        nb_satellites = int(fichier_input.readline().rstrip())
+
+        liste_satellites = []  # On transforme chaque ligne en une instance de la classe Satellite
+        for i in range(0, nb_satellites):
+            chaine = fichier_input.readline().rstrip()
+            satellite = self.satellite_par_chaine(chaine, nb_tours)
+            liste_satellites.append(satellite)
+
+        # On transforme les lignes suivantes en instances des classes Photo et Collection
+
+
+
+
+
         fichier_input.close()
 
-        instances_sat = self.creation_satellites(int(nombre_satellites))
-        return nb_tours, nb_satellites, instances_sat
+        return nb_tours, nb_satellites, liste_satellites
 
-    def creation_satellites(self, nombre):
-        """
-           Méthode chargée de créer les instances de satellites.
-           nombre est un entier correspondant au nombre de satellites.
-           La fonction retourne une liste contenant les instances de Satellite.
-        """
-
-        fichier_input = open(self.chemin_input, 'r')
-        liste_satellites = []
-        for i in range(0, 2 + nombre):
-            if i == 0 or i == 1:  # On passe les deux premières lignes avant la création.
-                fichier_input.readline()
-            else:
-                chaine = fichier_input.readline().rstrip()
-                satellite = self.satellite_par_chaine(chaine)
-                liste_satellites.append(satellite)
-
-        return liste_satellites
-
-    def satellite_par_chaine(self, caracteres):
+    def satellite_par_chaine(self, caracteres, nb_tours):
         """"Transforme une ligne du fichier input en une instance de la classe Satellite"""
-        liste_arguments = [1, 2, 3, 4, 5]  # On doit donner 5 arguments à Satellite pour la création d'une instance
+        liste_arguments = [1, 2, 3, 4, 5,
+                           nb_tours]  # On doit donner 5 arguments à Satellite pour la création d'une instance
         num_liste = 0  # ième argument de la liste
         argument = ""
         for j in range(len(caracteres)):
             if caracteres[j] != " ":
                 argument += caracteres[j]
             else:  # Ici, l'argument est ajouté à liste_arguments
-                liste_arguments[num_liste] = argument
+                liste_arguments[num_liste] = int(argument) #On doit transformer chaque information en entier
                 num_liste += 1
                 argument = ""
         liste_arguments[num_liste] = argument  # Comme la ligne ne se termine pas par un espace, on rajoute le dernier
 
         return Satellite(liste_arguments[0], liste_arguments[1], liste_arguments[2], liste_arguments[3],
-                         liste_arguments[4])
+                         liste_arguments[4], liste_arguments[5])
 
 
 parseur = Parseur()
-nombre_tours, nombre_satellites, liste = parseur.recup()
+nombre_tours, nombre_satellites, liste_satellites = parseur.recup()
