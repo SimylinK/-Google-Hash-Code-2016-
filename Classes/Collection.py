849729f2fe9951912d9e753e33eaca2091b86103
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2016-11-25 15:06:33 +0100

    Corrections et ajout de la création de Photos dans classe Collection

--- Classes/Collection.py
+++ Classes/Collection.py
@@ -16,15 +16,28 @@ class Collection:
         points qui lui sont associés. 
     """
 
-    def __init__(self, nb_points, nb_photos):
+    def __init__(self, nb_points, nb_photos,nb_intervalles,):
         self.nb_points = nb_points
         self.nb_photos = nb_photos
-        self.ratio_rentabilité = self.donner_ratio()
+        self.nb_intervalles = nb_intervalles
+        self.liste_photos = []  # Liste d'instances de la classe Photo, qu'on initialise vide et remplit ensuite
+        self.ratio_rentabilité = self.donner_ratio(nb_points,nb_photos)
 
-    def donner_ratio(self):
+    def donner_ratio(self,nb_points,nb_photos):
         """
             Méthode chargée de :
             Calculer le ratio de rentabilité.
         """
         ratio = nb_points/nb_photos
         return ratio
+
+    def ajouter_photo(self,latitude,longitude):
+        """
+        :param latitude:
+        :param longitude:
+
+            Méthode qui crée une instance de la classe Photo et l'ajoute à la collection.
+        """
+        photo = Photo(latitude,longitude)
+        self.liste_photos.append(photo)
+
