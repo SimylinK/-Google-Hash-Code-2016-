848400a5fd9b95affe1100f5817371053c2a7fa0
E146187Z <brewal.henaff@etu.univ-nantes.fr>
2016-11-29 11:55:54 +0100

    Ajout méthode choix_collections dans Distributeur

--- Classes/Distributeur.py
+++ Classes/Distributeur.py
@@ -17,8 +17,9 @@ class Distributeur:
     distribuer les photos entre les satellites et des les associer dans un calendrier
     """
 
-    def __init__(self, nb_tours, liste_collections, liste_satellites):
+    def __init__(self, nb_tours, nb_satellites, liste_satellites, liste_collections):
         self.nb_tours = nb_tours
+        self.nb_satellites = nb_satellites
         self.liste_satellites = liste_satellites
         self.liste_collections = liste_collections  # Liste de toutes les collections
         self.liste_collections_choisies = []  # Liste des collections sélectionnées
@@ -26,13 +27,18 @@ class Distributeur:
 
     def choix_collections(self):
         """
-        Méthode qui permet de choisir quelles collections seront choisies pour être photographiées
+        Méthode qui permet d'ordonner les collections par importance
         """
-        return None
+        self.liste_collections_choisies = sorted(self.liste_collections, key=lambda x: x.ratio_rentabilite, reverse=True)
+
+        return self.liste_collections_choisies
 
     def creer_calendrier(self, tour, longitude, latitude, photo):
         """
-        :param photo : instance de Photo
+        :param tour:
+        :param longitude:
+        :param latitude:
+        :param photo:
 
         Méthode qui permet d'instancier un calendrier
         """
@@ -46,7 +52,7 @@ class Distributeur:
 
     def algo_opti(self):
         """
-        Méthode principal du programme
+        Méthode principale du programme
         celle qui va distribuer les photos aux satellites
         """
         return None
