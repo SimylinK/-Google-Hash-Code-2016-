315ad3f12f68ea0e07877d666f05c55914d9dd34
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2016-12-02 14:20:47 +0100

    Correction méthode déplacement caméra dans Satellite

--- Classes/Satellite.py
+++ Classes/Satellite.py
@@ -23,19 +23,18 @@ class Satellite:
         et la prochaine position de la caméra si besoin ( si les valeurs sont différentes de None)
         """
         # Déplacement du Satellite
-        vitesse = self.vitesse
-        lat = self.latitude + vitesse
+        lat = self.latitude + self.vitesse
         long = self.longitude - 15
         # Passage au-dessus du pôle nord
         if lat > 324000:
             lat = 324000 - (lat - 324000)
             long -= 648000
-            vitesse = -vitesse
+            self.vitesse = -self.vitesse
         # Passage au-dessus du pôle sud
         elif lat < -324000:
             lat = -324000 + (abs(lat) - 324000)
             long -= 648000
-            vitesse = -vitesse
+            self.vitesse = -self.vitesse
         # long est compris entre -648000 et 647999
         if long < -648000:
             long = 648000 - (-long - 648000)
@@ -43,10 +42,24 @@ class Satellite:
         self.longitude = long
 
         # Déplacement de la caméra
-        self.latitude_camera += vitesse
         if latitude_cam:
             self.latitude_camera = latitude_cam
-
-        self.longitude_camera -= 15
-        if longitude_cam:
             self.longitude_camera = longitude_cam
+        else:
+            lat_cam = self.latitude_camera + self.vitesse
+            long_cam = self.longitude_camera - 15
+
+            # Passage au-dessus du pôle nord
+            if lat_cam > 324000:
+                lat_cam = 324000 - (lat_cam - 324000)
+                long_cam -= 648000
+            # Passage au-dessus du pôle sud
+            elif lat_cam < -324000:
+                lat_cam = -324000 + (abs(lat_cam) - 324000)
+                long_cam -= 648000
+            # long est compris entre -648000 et 647999
+            if long_cam < -648000:
+                long_cam = 648000 - (-long_cam - 648000)
+
+            self.latitude_camera = lat_cam
+            self.longitude_camera = long_cam
