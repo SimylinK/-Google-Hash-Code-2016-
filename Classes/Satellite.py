e9191599fd3533af4f85f4f3977e397c68fd0092
Nathan Maraval <nathan.maraval@etu.univ-nantes.fr>
2016-12-03 22:59:34 +0100

    Satelite : calcul distances satelite-camera

--- Classes/Satellite.py
+++ Classes/Satellite.py
@@ -64,3 +64,35 @@ class Satellite:
 
             self.latitude_camera = lat_cam
             self.longitude_camera = long_cam
+
+    def calcul_distance_satellite_camera(self):
+        """ Calcul la distance entre le satellite et la position de la caméra
+        Attention ne marche pas si a côté d'un pôle
+        :return: 2 entiers positifs, (latitude, longitude)
+        """
+        """Calcul de la distance en longitude"""
+        if (abs(self.longitude_camera - self.longitude) > self.max_deplacement_camera):
+            """ Dans ce cas on passe par -648000 et 647999 en longitude """
+            if (self.longitude_camera < self.longitude):
+                dist_long = self.longitude_camera + 1296000 - self.longitude
+            else:
+                dist_long = self.longitude + 1296000 - self.longitude_camera
+        else:
+            dist_long = abs(self.longitude - self.longitude_camera)
+
+        """Calcul de la distance en latitude"""
+        distance_lat = abs(self.latitude - self.latitude_camera)
+
+        return distance_lat, dist_long
+
+
+if(__name__ == "__main__"):
+    # Création d'un satellite
+    s1 = Satellite(0, 0, -648000, 10, 5, 100)
+    s1.latitude_camera = 6
+    s1.longitude_camera = 647998
+
+    # Test calcul_distance_satellite_camera
+    lat, long = s1.calcul_distance_satellite_camera()
+    print("lat = " + str(lat) + " ; (doit être égale a 6)")
+    print("long = " + str(long) + " ; (doit être égale a 2)")
