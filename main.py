bc50b1b00163044ae963d028c637a90aea06e91e
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2016-12-12 00:49:35 +0100

    Fix pointeur de liste_zones

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import time
 
 if __name__ == '__main__':
     time.clock()
-    parseur = Parseur('/donneesTest/weekend.in')
+    parseur = Parseur('/donneesTest/constellation.in')
     nombre_tours, nombre_satellites, liste_satellites, liste_collections, globe = parseur.initialisation()
     distrib = Distributeur(nombre_tours, nombre_satellites, liste_satellites, liste_collections, globe)
     nb_photos_prises = distrib.algo_opti()
-    parseur.creer_output(globe.liste_zones, nb_photos_prises)
+    parseur.creer_output(distrib.globe.liste_zones, nb_photos_prises)
     temps_exec = time.clock()
     if temps_exec <= 60:
         print("Le temps d'exécution fut de " + str(temps_exec) + " secondes")
